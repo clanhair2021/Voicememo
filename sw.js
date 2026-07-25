16c3b620fb922c1be3cd52aa444524dd805d61ee
clanhair2021 <clan.hair.2021@gmail.com>
2026-07-25 18:06:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-// Service Worker (ダミー記述でもPWAとして認識されます)
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  // 通常の通信をそのまま通す
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
 });
 
+self.addEventListener('fetch', (e) => {
+  // Supabaseや外部CDNの通信を邪魔せずそのまま通過させる
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
